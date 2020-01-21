a368e8b424d45a7e101b9f8ed670a0dea5a1a6b2
parham <prhmma@gmail.com>
2020-01-21 21:58:33 +0330

    this is not complete

--- src/Game.js
+++ src/Game.js
@@ -27,8 +27,6 @@ const machineWillWin = ( squares, lines, j ) => {
       const rest = line.filter( cellIndex => cellIndex === j )
       if ( rest.length !== 0 ) return true;
     } )
-    console.log( "i am in ", j );
-    console.log( targetLines );
     //calculate probabilities
     let flag = false;
     targetLines.forEach( line => {
@@ -36,10 +34,8 @@ const machineWillWin = ( squares, lines, j ) => {
       for ( let i = 0 ; i < 3 ; i++ ){
         if ( squares[ line[ i ] ] === 'O' ) sum++;
       } 
-      console.log( sum );
       if ( sum === 2 ) flag = true;
     });
-    console.log( " continue" )
     return flag;
 }
 
@@ -57,6 +53,29 @@ class Game extends React.Component {
     this.setState({
       stepNumber: step,
     });
+  }
+  moveMachine( lines, squares, i, j, p, k ){
+    if ( k === 9 ) return null;
+    i = Math.floor( Math.random() * p.length );
+    j = Math.floor( Math.random() * 3 );
+    console.log( i, j , "this one \n", p )
+    if ( i === NaN || j === NaN ) {
+      return null
+    }
+    while ( squares[ p[ i ].cells[ j ] ] === 'T' ) {
+        console.log( '1' )
+        i = Math.floor( Math.random() * p.length );
+        j = Math.floor( Math.random() * 3 );   
+        console.log( i, j , "this two \n", p )   
+    }
+    let rest
+    if (  machineWillWin( squares, lines, p[ i ].cells[ j ] ) ) {
+      rest = this.moveMachine( squares, i, j, p, k + 1 )
+      if ( !rest ) return null
+      else return [ i, j ]
+    }
+    else return [ i, j ]
+  
   }
   nextMachineMove(squares){
     const lines = [
@@ -76,54 +95,34 @@ class Game extends React.Component {
     lines.forEach( line => {
       let sum = 0;
       for ( let i = 0 ; i < line.length ; i++ ){
-        if ( squares[ line[ i ] ] === 'T' ) sum++;
-        if ( squares[ line[ i ] ] === 'O' ) {
-          sum = 0;
-          break;
-        }
+        if ( squares[ line[ i ] ] === 'O' ) sum++;
+        // if ( squares[ line[ i ] ] === 'X' ) {
+        //   sum = 0;
+        //   break;
+        // }
       } 
       probabilities.push({
         p : sum / 3,
         cells : line
       });
     });
-    
-    // calculate max probability
-    let maxProb = probabilities[ 0 ];
-    probabilities.forEach( line => {
-        if ( line.p > maxProb.p ) maxProb = line;
-    });  
-    
-    if ( maxProb.p === 1 ) return;
 
-    // calculate equal probabilities
-    const sameProbs = [];
-    probabilities.forEach( line => {
-      if ( line.p === maxProb.p ) sameProbs.push( line ); 
-    });
-    
-    // check if center was selected
-    if ( !squares[ 4 ] ){
-      squares [ 4 ] = 'O';
-      // update state
-      this.setState( prevState => ( {
-        history: prevState.history.concat([{
-         squares: squares 
-        }]),
-        stepNumber : ++prevState.stepNumber
-      } ) )
-      return;
+    let p0 = probabilities.filter( line => line.p === 0 );
+    const ans = this.moveMachine( lines, squares, null, null, p0 ,0 );
+    if ( !ans ){
+      let p1 = probabilities.filter( line => line.p === 1 / 3 );
+      const ans = this.moveMachine( lines, squares, null, null, p1, 0 );
+      if ( !ans ){
+        console.log( 'nothing was found ' );
+        return;
+      }
+      let [ i, j ] = ans;
+      squares[ p1[ i ].cells[ j ] ] = 'O';
     }
-    
-    // machine move
-    let i = Math.floor( Math.random() * sameProbs.length );
-    let j = Math.floor( Math.random() * 3 );
-    while ( squares[ sameProbs[ i ].cells[ j ] ] === 'T' || machineWillWin( squares, lines, sameProbs[ i ].cells[ j ] ) ) {
-        i = Math.floor( Math.random() * sameProbs.length );
-        j = Math.floor( Math.random() * 3 );      
-    }
-    squares[ sameProbs[ i ].cells[ j ] ] = 'O';
-    
+    let [ i, j ] = ans;
+    squares[ p0[ i ].cells[ j ] ] = 'O';
+
+   
     // update state
     this.setState( prevState => ( {
       history: prevState.history.concat([{
@@ -131,6 +130,68 @@ class Game extends React.Component {
       }]),
       stepNumber : ++prevState.stepNumber
     } ) )
+
+
+
+
+// try {
+//   probabilities.forEach( line => {
+//     if ( typeof( line.p ) === undefined ) throw "this is error" 
+//   } )
+// }
+// catch( error ){
+//   console.log( error )
+// }
+
+    // console.log( probabilities )
+    // calculate max probability
+    // let index = Math.floor( Math.random() * 9 );
+    // let minProb = probabilities[ 0 ];
+    // probabilities.forEach( line => {
+    //     if ( line.p < minProb.p ) minProb = line;
+    // });  
+    // console.log( minProb )
+    // // calculate equal probabilities
+    // const sameProbs = [];
+    // probabilities.forEach( line => {
+    //   if ( line.p === minProb.p ) sameProbs.push( line ); 
+    // });
+    // console.log( probabilities )
+    // check if center was selected
+    // if ( !squares[ 4 ] ){
+    //   squares [ 4 ] = 'O';
+    //   // update state
+    //   this.setState( prevState => ( {
+    //     history: prevState.history.concat([{
+    //      squares: squares 
+    //     }]),
+    //     stepNumber : ++prevState.stepNumber
+    //   } ) )
+    //   return;
+    // }
+    
+    // machine move
+    // let i = Math.floor( Math.random() * sameProbs.length );
+    // let j = Math.floor( Math.random() * 3 );
+    // while ( squares[ sameProbs[ i ].cells[ j ] ] === 'T' || machineWillWin( squares, lines, sameProbs[ i ].cells[ j ] ) ) {
+    //     console.log( '1' )
+    //     i = Math.floor( Math.random() * sameProbs.length );
+    //     j = Math.floor( Math.random() * 3 );      
+    // }
+    // let rest;
+    // if ( machineWillWin( squares, lines, sameProbs[ i ].cells[ j ] ) ) {
+    //     rest = probabilities.filter( line => line.p === 1 / 3  )
+    //     i = Math.floor( Math.random() * rest.length );
+    //     j = Math.floor( Math.random() * 3 );
+    //     console.log( '2' )
+    //     while ( machineWillWin( squares, lines, squares[ rest[ i ].cells[ j ] ] ) || squares[ rest[ i ].cells[ j ] ] === 'T' || squares[ rest[ i ].cells[ j ] ] === 'O' ){
+    //         console.log( '3' )
+    //         i = Math.floor( Math.random() * rest.length );
+    //         j = Math.floor( Math.random() * 3 );
+    //     }
+    // }
+    // if ( !rest ) 
+    // else squares[ rest[ i ].cells[ j ] ] = 'O';
 
   }
   handleClick(i) {
